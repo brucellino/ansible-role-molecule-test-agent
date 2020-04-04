f252e0ea0a94dd6f9a0ed4c751572c59eb581bba
Bruce Becker <brucellino@gmail.com>
2020-04-04 11:07:09 +0200

    Update compliance profile into required and optional for EC2
    This separates the list of packages used as baseline into required and
    optional packages.
    
    The compliance profile is updated.
    
    Extra tasks have been added for installing Docker CE on EC2 hosts
    
    Signed-off-by: Bruce Becker <brucellino@gmail.com>

--- molecule/default/tests/test_baseline_packages.py
+++ molecule/default/tests/test_baseline_packages.py
@@ -12,7 +12,7 @@ def required_packages():
     """
     Reads input.yml file and returns the list for the fixture
     """
-    with open(r"tests/input.yml") as file:
+    with open(r"required-packages.yml") as file:
         inputs = yaml.load(file, Loader=yaml.FullLoader)
     return inputs["required_packages"]
 
@@ -24,7 +24,17 @@ def optional_packages():
     """
     with open(r"tests/input.yml") as file:
         inputs = yaml.load(file, Loader=yaml.FullLoader)
-    return inputs["required_packages"]
+    return inputs["optional_packages"]
+
+
+def python_packages():
+    """
+    Reads input.yml and returns a list of python
+    related packages
+    """
+    with open(r"tests/input.yml") as file:
+        inputs = yaml.load(file, Loader=yaml.FullLoader)
+    return inputs["python_packages"]
 
 
 @pytest.mark.parametrize("package_name", required_packages())
@@ -32,6 +42,6 @@ def test_required_packages(host, package_name):
     assert host.package(package_name).is_installed
 
 
-@pytest.mark.parametrize("package_name", optional_packages())
-def test_optional_packages(host, package_name):
-    assert host.package(package_name).is_installed
+# @pytest.mark.parametrize("package_name", optional_packages())
+# def test_optional_packages(host, package_name):
+#     assert host.package(package_name).is_installed
