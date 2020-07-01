52732ae2147f80dc69e25de51210d57911ec305e
Bruce Becker <brucellino@gmail.com>
2020-07-01 10:13:50 +0200

    Add github_changelog_generator
    Signed-off-by: Bruce Becker <brucellino@gmail.com>

--- molecule/default/tests/test_jenkins.py
+++ molecule/default/tests/test_jenkins.py
@@ -1,3 +1,5 @@
+# Tests of the execution environment that jenkins jobs
+# will execute in
 import os
 import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -6,5 +8,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_jenkins_user(host):
+    '''
+    Jenkins user should exist
+    '''
     jenkins_user = host.user('jenkins')
     assert jenkins_user.name == 'jenkins'
+
+
+def test_step_executables(host):
+    '''
+    Executables required in Jenkins steps should be available
+    '''
+
+    assert host.exists('github_changelog_generator')
+    assert host.exists('rubocop')
